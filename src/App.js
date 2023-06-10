df4f3f1bbe8c5c16dfa07ad003da55ee437f898f
Srishti Gautam <82943455+srishtigautam21@users.noreply.github.com>
2023-06-10 12:54:21 +0530

    Feat:Chrome extension

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import MainPage from "./component/MainPage";
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [imageUrl, setImageUrl] = useState(null);
+  let URL =
+    "https://api.unsplash.com/photos/random/?client_id=zJ9f9avxrS-Tt2UYC4hK1eh7vRmny7QUiLxMQN9l4S8&&orientation=landscape&&query=travel%20dark";
+  const fetchUrl = async (url) => {
+    try {
+      let response = await fetch(url);
+      let data = await response.json();
+      setImageUrl(data?.urls?.regular);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  useEffect(() => {
+    fetchUrl(URL);
+    // eslint-disable-next-line
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    >
+      <MainPage />
     </div>
   );
 }
